7bd5e83c532c5d1b70cfb241bfc0a48382f15216
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 16:26:20 +0600

    feat(profile): create UserModel for user profile data

---
+++ lib/View/Screen/Profile/model/user_model.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  final String id;
+  final String name;
+  final String email;
+  final String phone;
+  final String avatarUrl;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.avatarUrl,
+  });
+}

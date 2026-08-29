f32715b0447078ea0e776f1df3b3c8f8824ce0b6
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 10:42:30 +0600

    feat(assets): register SVG asset icons for Splash, Start Now, Welcome, and Social logins

--- lib/Utils/AppIcons/app_icons.dart
+++ lib/Utils/AppIcons/app_icons.dart
@@ -1,6 +1,15 @@
 class AppIcons {
   static const String basePath = 'assets/icons';
 
+  // SVG Icons from Design
+  static const String splashIcon = '$basePath/Splash-Icons.svg';
+  static const String startNowIcon = '$basePath/Start Now-Icons.svg';
+  static const String welcomeIcon = '$basePath/WELCOME!-Icons.svg';
+  static const String appleIcon = '$basePath/Apple-Icons.svg';
+  static const String facebookIcon = '$basePath/Facbook-Icons.svg';
+  static const String googleIcon = '$basePath/Google-icons.svg';
+
+  // Additional App Icons
   static const String coffee = '$basePath/coffee.png';
   static const String home = '$basePath/home.png';
   static const String search = '$basePath/search.png';

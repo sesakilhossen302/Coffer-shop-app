70e6ddbca299fef40c3331a1ab66ad4f3dcaf4fc
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 10:29:30 +0600

    fix(android): update app compileSdk to 36 to satisfy AndroidX requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.coffee_shop_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {

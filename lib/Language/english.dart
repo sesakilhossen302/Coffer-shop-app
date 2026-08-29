ca01a843cfa37f61cfb163c6b02438b296259650
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 10:13:51 +0600

    feat(lang): add English translations map

---
+++ lib/Language/english.dart
@@ -0,0 +1,13 @@
+Map<String, String> englishMap = {
+  'coffee_shop': 'Coffee Shop',
+  'welcome_text': 'Find the best coffee for you',
+  'search_hint': 'Search coffee...',
+  'buy_now': 'Buy Now',
+  'add_to_cart': 'Add to Cart',
+  'price': 'Price',
+  'description': 'Description',
+  'size': 'Size',
+  'splash_tagline': 'Crafted with Passion & Roasted to Perfection',
+  'no_internet': 'No internet connection',
+  'something_went_wrong': 'Something went wrong. Please try again.',
+};

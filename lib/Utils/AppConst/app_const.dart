0f5a07c6766c213391898be9eeac4dd07f035a67
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 10:13:35 +0600

    feat(utils): add AppConst layout metrics and storage keys

---
+++ lib/Utils/AppConst/app_const.dart
@@ -0,0 +1,17 @@
+class AppConst {
+  static const String appName = 'Coffee Shop App';
+  static const String appVersion = '1.0.0';
+
+  // Storage Keys
+  static const String tokenKey = 'user_auth_token';
+  static const String userIdKey = 'user_id';
+  static const String userEmailKey = 'user_email';
+  static const String languageCodeKey = 'language_code';
+  static const String countryCodeKey = 'country_code';
+  static const String isDarkModeKey = 'is_dark_mode';
+
+  // Layout Constants
+  static const double defaultPadding = 16.0;
+  static const double defaultRadius = 12.0;
+  static const double buttonHeight = 52.0;
+}

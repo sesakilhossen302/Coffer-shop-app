5654be0817397cbc90187583697c1411cbcfd0fb
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 11:40:33 +0600

    feat(assets): register My Cart-Img.png image asset

--- lib/Utils/AppImg/app_img.dart
+++ lib/Utils/AppImg/app_img.dart
@@ -2,6 +2,7 @@ class AppImg {
   static const String basePath = 'assets/images';
 
   static const String coinImg = '$basePath/Home-Page-Coin-img.png';
+  static const String myCartImg = '$basePath/My Cart-Img.png';
   static const String splashLogo = '$basePath/splash_logo.png';
   static const String coffeeBanner = '$basePath/coffee_banner.png';
   static const String placeholder = '$basePath/placeholder.png';

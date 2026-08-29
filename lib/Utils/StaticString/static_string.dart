cb732e3b323fa833c56b1812bf1f552bf8619d83
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 10:13:46 +0600

    feat(utils): add StaticString text keys

---
+++ lib/Utils/StaticString/static_string.dart
@@ -0,0 +1,17 @@
+class StaticString {
+  static const String appTitle = 'Coffee Shop';
+  static const String welcomeText = 'Find the best coffee for you';
+  static const String searchCoffeeHint = 'Search coffee...';
+  static const String buyNow = 'Buy Now';
+  static const String addToCart = 'Add to Cart';
+  static const String price = 'Price';
+  static const String description = 'Description';
+  static const String size = 'Size';
+  static const String small = 'S';
+  static const String medium = 'M';
+  static const String large = 'L';
+  static const String noInternet = 'No internet connection';
+  static const String somethingWentWrong = 'Something went wrong. Please try again.';
+  static const String success = 'Success';
+  static const String error = 'Error';
+}

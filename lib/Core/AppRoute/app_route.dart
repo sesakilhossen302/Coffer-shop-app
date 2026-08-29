b3a624079d3f23ae911a83fed2ce20b96d762055
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 10:36:43 +0600

    feat(routes): register loginScreen in AppRoute navigation system

--- lib/Core/AppRoute/app_route.dart
+++ lib/Core/AppRoute/app_route.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
+import '../../View/Screen/Auth/login_screen.dart';
 import '../../View/Screen/LoadingScreen/loading_screen.dart';
 import '../../View/Screen/SplashScreen/splash_screen.dart';
 
 class AppRoute {
   static const String loadingScreen = '/loading';
   static const String splashScreen = '/splash';
+  static const String loginScreen = '/login';
   static const String homeScreen = '/home';
 
   static List<GetPage> routes = [
@@ -16,6 +18,11 @@ class AppRoute {
     GetPage(
       name: splashScreen,
       page: () => const SplashScreen(),
+      transition: Transition.rightToLeftWithFade,
+    ),
+    GetPage(
+      name: loginScreen,
+      page: () => const LoginScreen(),
       transition: Transition.rightToLeftWithFade,
     ),
   ];

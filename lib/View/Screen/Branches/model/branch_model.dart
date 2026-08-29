34b52ee9ade169151a57cd5d3257428e6adda7d1
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 15:40:23 +0600

    feat(branches): add isOpen, isFavorite, and logoUrl properties to BranchModel

--- lib/View/Screen/Branches/model/branch_model.dart
+++ lib/View/Screen/Branches/model/branch_model.dart
@@ -4,11 +4,14 @@ class BranchModel {
   final String address;
   final String phone;
   final String mapImageUrl;
+  final String logoUrl;
   final String canPayInCash;
   final String rewardsExpire;
   final String whatsappUrl;
   final String instagramUrl;
   final String facebookUrl;
+  final bool isOpen;
+  bool isFavorite;
 
   BranchModel({
     required this.id,
@@ -16,10 +19,13 @@ class BranchModel {
     required this.address,
     required this.phone,
     required this.mapImageUrl,
+    this.logoUrl = 'https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?w=150&q=80',
     this.canPayInCash = 'Yes',
     this.rewardsExpire = 'No',
     this.whatsappUrl = '',
     this.instagramUrl = '',
     this.facebookUrl = '',
+    this.isOpen = true,
+    this.isFavorite = false,
   });
 }

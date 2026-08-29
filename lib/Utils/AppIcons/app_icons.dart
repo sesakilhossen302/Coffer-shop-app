0b9dc61729a118016def847d984bf3e8bdf9f52d
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 10:13:39 +0600

    feat(utils): add AppIcons asset path constants

---
+++ lib/Utils/AppIcons/app_icons.dart
@@ -0,0 +1,13 @@
+class AppIcons {
+  static const String basePath = 'assets/icons';
+
+  static const String coffee = '$basePath/coffee.png';
+  static const String home = '$basePath/home.png';
+  static const String search = '$basePath/search.png';
+  static const String cart = '$basePath/cart.png';
+  static const String favorite = '$basePath/favorite.png';
+  static const String user = '$basePath/user.png';
+  static const String back = '$basePath/back.png';
+  static const String notification = '$basePath/notification.png';
+  static const String star = '$basePath/star.png';
+}

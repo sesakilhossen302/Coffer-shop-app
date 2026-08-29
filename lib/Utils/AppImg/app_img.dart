c754aedf624fb4ab058bd6f0f4bc9cf38844892c
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 10:13:43 +0600

    feat(utils): add AppImg asset path constants

---
+++ lib/Utils/AppImg/app_img.dart
@@ -0,0 +1,9 @@
+class AppImg {
+  static const String basePath = 'assets/images';
+
+  static const String splashLogo = '$basePath/splash_logo.png';
+  static const String coffeeBanner = '$basePath/coffee_banner.png';
+  static const String placeholder = '$basePath/placeholder.png';
+  static const String profileAvatar = '$basePath/profile_avatar.png';
+  static const String emptyCart = '$basePath/empty_cart.png';
+}

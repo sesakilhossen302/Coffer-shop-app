95e0f21e4f1df5eb61f5d316dead47a189dc0486
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-08-29 10:25:03 +0600

    feat(routes): register loadingScreen and splashScreen in AppRoute navigation system

--- lib/Core/AppRoute/app_route.dart
+++ lib/Core/AppRoute/app_route.dart
@@ -1,15 +1,22 @@
 import 'package:get/get.dart';
+import '../../View/Screen/LoadingScreen/loading_screen.dart';
 import '../../View/Screen/SplashScreen/splash_screen.dart';
 
 class AppRoute {
+  static const String loadingScreen = '/loading';
   static const String splashScreen = '/splash';
   static const String homeScreen = '/home';
 
   static List<GetPage> routes = [
     GetPage(
+      name: loadingScreen,
+      page: () => const LoadingScreen(),
+      transition: Transition.fadeIn,
+    ),
+    GetPage(
       name: splashScreen,
       page: () => const SplashScreen(),
-      transition: Transition.fadeIn,
+      transition: Transition.rightToLeftWithFade,
     ),
   ];
 }
